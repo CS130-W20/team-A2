b2447ddc4d5bade3ee4ee49fd1190a21fcacbb1c
pravin <pravin@risksciences.ucla.edu>
2020-02-18 18:07:55 -0800

    Added base User Login Screen

---
+++ Spur/screens/UserLoginScreen.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { Image,
+	 Platform,
+	 StyleSheet,
+	 Text,
+	 TextInput,
+	 TouchableOpacity,
+	 View } from 'react-native';
+
+export default class UserLoginScreen extends Component<Props>
+{
+
+    render() {
+	return (
+		<View style={styles.container}>
+		<Text>Create Event</Text>
+		</View>
+  	);
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  activeTitle: {
+    color: 'red',
+  },
+});
