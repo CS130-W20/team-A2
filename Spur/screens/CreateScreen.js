51087df2044757714ab8a42883b4c3e3f3c74449
Timothy Margono <sleepincot@ucla.edu>
2020-02-20 16:31:19 -0800

    Merging create_event branch for Part C Milestone (#14)
    * Add Google Maps to CreateScreen.js
    
    * Add styling to better fit text input with the map
    
    * Add Scrolling, more text fields
    
    * Work on styling, add submit button
    
    * Added Input Validation, Draggable Map Markers for location, Modified handleChange functions, Finished Submit Button, Added comments for documentation, Removed old code
    
    Added Input Validation done with regex,
    Draggable Map Markers for location, Modified handleChange functions to take strings rather than events, Finished Submit Button, Added comments for documentation generator, Removed old code
    
    * Fixed a bug in Input Validation
    
    Changed regex quantifier to make sure that a party and cost value exist

--- Spur/screens/CreateScreen.js
+++ Spur/screens/CreateScreen.js
@@ -1,161 +1,268 @@
 import React, {Component} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import { Image,
 	 Platform,
 	 StyleSheet,
 	 Text,
 	 TextInput,
 	 TouchableOpacity,
-	 View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+	 View,
+     ScrollView,
+	 SafeAreaView,
+	 Button,
+	 Alert
+	} from 'react-native';
+
 import * as WebBrowser from 'expo-web-browser';
 
 import { MonoText } from '../components/StyledText';
+import {KeyboardAvoidingView} from 'react-native';
 
-export default class HomeScreen extends Component<Props> {
-    state = {
-	name: ''
-    };
+export default class CreateScreen extends Component<Props> {
+    constructor(props) {
+		super(props);
+		this.state = {
+		name: '',
+		cost: '',
+		party: '',
+		desc: '',
+		startTime: '',
+		endTime: '',
+		category: '',
+		date: '',
+		region: {
+		  latitude: 37.78825,
+		  longitude: -122.4324,
+		},
+		};
+	
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleDateChange = this.handleDateChange.bind(this);
+		this.handleStartTimeChange = this.handleStartTimeChange.bind(this);
+		this.handleEndTimeChange = this.handleEndTimeChange.bind(this);
+		this.handleCategoryChange = this.handleCategoryChange.bind(this);
+		this.handleCostChange = this.handleCostChange.bind(this);
+		this.handlePartyChange = this.handlePartyChange.bind(this);
+		this.handleDescChange = this.handleDescChange.bind(this);
+	};
+	/** Updates name state variable when input is detected.
+	* @param {string} Event Name
+	*/
     handleNameChange = e => {
 	this.setState({
-	    name: e.nativeEvent.text
+	    name: e
 	});
     };
+	/** Updates date state variable when input is detected.
+	* @param {string} Date
+	*/
+	handleDateChange = e => {
+	this.setState({
+	    date: e
+	});
+    };
+	/** Updates start time state variable when input is detected.
+	* @param {string} Start Time
+	*/
+	handleStartTimeChange = e => {
+	this.setState({
+	    startTime: e
+	});
+    };
+	/** Updates end time state variable when input is detected.
+	* @param {string} End Time
+	*/
+	handleEndTimeChange = e => {
+	this.setState({
+	    endTime: e
+	});
+    };
+	/** Updates category state variable when input is detected.
+	* @param {string} Category
+	*/
+	handleCategoryChange = e => {
+	this.setState({
+	    category: e
+	});
+    };
+	/** Updates cost state variable when input is detected.
+	* @param {string} Cost
+	*/
+	handleCostChange = e => {
+	this.setState({
+	    cost: e
+	});
+    };
+	/** Updates party state variable when input is detected.
+	* @param {string} Party Size
+	*/
+	handlePartyChange = e => {
+	this.setState({
+	    party: e
+	});
+    };
+	/** Updates description state variable when input is detected.
+	* @param {string} Description
+	*/
+	handleDescChange = e => {
+	this.setState({
+	    desc: e
+	});
+    };
+	/** Validates event details
+	* @param {event} React Native Event
+	*/
+	handleSubmit(event) {
+		var datePattern = /[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]/;
+		var timePattern = /[0-9][0-9]:[0-9][0-9]/;
+		var partyPattern = /[0-9]+/;
+		var costPattern = /[0-9]+/;
+		if (!timePattern.test(this.state.startTime) | !timePattern.test(this.state.endTime)) {
+		  Alert.alert("Invalid Start or End Time");
+		}
+		else if (!datePattern.test(this.state.date)) {
+		  Alert.alert("Invalid Date");
+		}
+		else if (!partyPattern.test(this.state.party)) {
+		  Alert.alert("Invalid Party Size");
+		}
+		else if (!costPattern.test(this.state.cost)) {
+		  Alert.alert("Invalid Cost");
+		}
+		else if (this.state.name == '') {
+		  Alert.alert("Invalid Event Name");
+		}
+		else if (this.state.category == '') {
+		  Alert.alert("Invalid Category");
+		}
+		else if (this.state.desc == '') {
+		  Alert.alert("Invalid Event Description");
+		}
+		else {
+		  Alert.alert("Success");
+		}
+	};
+	/** Renders the UI shown to the user.
+	*/
     render() {
 	return (
-		<View style={styles.container}>
+    <View style={styles.container}>
+		<MapView
+		  provider={PROVIDER_GOOGLE}
+		  style={styles.map}
+          initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          }} 
+		>
+		<Marker draggable coordinate={this.state.region} onDragEnd={(e) => this.setState({region: e.nativeEvent.coordinate})}
+		/>
+        </MapView>
 		
-		<View style={styles.getStartedContainer}>
-		<Text>Create Event</Text>
-		<TextInput onChange={this.handleNameChange} defaultValue={'Name'} clearTextOnFocus={true}/>
-		<Text>{this.state.name}</Text>
-	        </View>
+		<View style={{flex: 1}}>
+        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Event Name</Text>
+		<TextInput style={styles.nameText} onChangeText={this.handleNameChange} clearTextOnFocus={true}/>
 		</View>
+		
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Event Date</Text>
+		<TextInput style={styles.nameText} onChangeText={this.handleDateChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Start Time</Text>
+		<TextInput style={styles.input} onChangeText={this.handleStartTimeChange} clearTextOnFocus={true}/>
+		<Text style={styles.formText}>End Time</Text>
+		<TextInput style={styles.input} onChangeText={this.handleEndTimeChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Cost</Text>
+		<TextInput style={styles.input} onChangeText={this.handleCostChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Party Size</Text>
+		<TextInput style={styles.input} onChangeText={this.handlePartyChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<View style={styles.textContainer}>
+		<Text style={styles.formText}>Category</Text>
+		<TextInput style={styles.input} onChangeText={this.handleCategoryChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<Text style={styles.formText}>Description</Text>
+		<View style={styles.textContainer}>
+		<TextInput style={styles.descriptionText} onChangeText={this.handleDescChange} clearTextOnFocus={true}/>
+		</View>
+		
+		<Button title="Submit" onPress={this.handleSubmit}/>
+	    </ScrollView>
+		</View>
+
+    </View>
   );
     }
 }
 
-HomeScreen.navigationOptions = {
+CreateScreen.navigationOptions = {
   header: null,
 };
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+  map: {
+	height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',	
+  },
+  inputContainer: {
+	borderWidth: 1,
+	borderColor: 'lightgrey',
+    height: 50,
+  },
+  input: {
+	height: 30,
+    backgroundColor: 'lightgrey',
+    paddingLeft: 15,
+    paddingRight: 15,	
+  },
+  nameText: {
+	height: 30,
+	width: 100,
+    backgroundColor: 'lightgrey',
+    paddingLeft: 15,
+    paddingRight: 15,	
+  },
+  formText: {
+    color: 'black',
+    fontSize: 20,
     textAlign: 'center',
+	margin: 5,
   },
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  descriptionText: {
+	height: 100,
+	width: 300,
+    backgroundColor: 'lightgrey',
+    paddingLeft: 15,
+    paddingRight: 15  
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  textContainer: {
+	flexGrow: 1,
+    justifyContent: 'center',
+	flexDirection: 'row',
+	padding: 5,
   },
 });
