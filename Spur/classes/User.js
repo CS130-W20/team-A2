9131e83a1e0b6b29e5a2fc2e421c31ad2ddb6d1b
truongdavid98 <truongdavid98@gmail.com>
2020-02-21 02:52:28 -0800

    Finished
    Finished the user profile mock up

--- Spur/classes/User.js
+++ Spur/classes/User.js
@@ -1,28 +1,21 @@
-import React, {Component} from 'react';
-import {
-	 StyleSheet,
-	 Text,
-	 View } from 'react-native';
-
-
 /**
  * User Class: Stores user information
  */
 export default class User {
     /**
-     * Constructor for the User
+     * User object holds user information
      * @constructor
      * @param {string} name - Name of the user
-     * @param {string} description - User written description. Initially empty.
-     * @param {Categories[]} interests - List of categories the user is interested in. Initially empty.
-     * @param {Event[]} history - List of events the user has attended. Initially empty. 
-     * @param {Event[]} upcoming - List of upcoming events the user has joined. Initially empty.
+     * @param {string} description - User written description.
+     * @param {Categories[]} interests - List of categories the user is interested in.
+     * @param {Event[]} history - List of events the user has attended. 
+     * @param {Event[]} upcoming - List of upcoming events the user has joined. 
      */
-    constructor(name) {
+    constructor(name, description = "", interests = [], history = [], upcoming = []) {
         this.name = name; 
-        this.description = ""; 
-        this.interests = []; 
-        this.history = [];
-        this.upcoming = [];
+        this.description = description; 
+        this.interests = interests;
+        this.history = history;
+        this.upcoming = upcoming; 
     }
 }
--- Spur/screens/ProfileScreen.js
+++ Spur/screens/ProfileScreen.js
@@ -2,38 +2,97 @@ import React, {Component} from 'react';
 import {
 	 StyleSheet,
 	 Text,
-	 View } from 'react-native';
-
-function getUser(props) {
-	//Dummy function to get a user
-	
-}
+	 View,
+	 ScrollView,
+	 Button,
+	 Alert } from 'react-native';
+import User from '../classes/User';
+import Event from '../classes/Event';
+import EventDetails from '../classes/EventDetails';
 
 /**
  * Profile Screen - Displays a user profile 
+ * @param {User} user - User whose profile will be displayed
  */
 export default class ProfileScreen extends Component<Props>
 {
-	itemId = this.props.
+	constructor(props) {
+		super(props); 
+
+		//Make a dummy user for now, will change later 
+		//Dummy event 1
+		var ed1 = new EventDetails("UML Appreciation Party", 
+		  "A day focused on the universal language",
+		  "4:00 pm", "5:50 pm", "Maged", "34.0727° N, 118.4393° W", 0, 100,
+		  ["UML", "Scrum", "Design"]
+		);
+		var env1 = new Event(ed1, ["Maged", "David", "Greg", "Pravin", "Tim", "Joseph", "Mihir"], 
+		"Chat", ["Tim", "Mihir"]
+		);
+		
+		//Dummy event 2
+		var ed2 = new EventDetails("Pizza Party", 
+		  "Delicious La Monica's", "10:00 pm", "11:00 pm", "Amit", 
+		  "34.0609° N, 118.4468° W", 3.65, 10, ["Food", "Social"]
+		);
+		var env2 = new Event(ed2, ["Maged", "Mihir", "Vignesh"], "Chat",
+		  ["Maged", "Mihir", "Vignesh"]
+		);
+		
+		//Dummy User 1
+		this.user = new User("Maged", "Professor for CS 130. Nasa JPL Senior software architect",
+		 ["Architecture", "Design", "UML", "Scrum", "Ducks", "Street Fighter", "Pizza"],
+		 [env1], [env2]
+		);
+	}
+
     render() {
 		return (
-			<View style={styles.container}>
-				<View style={styles.title}>
-					<Text>{this.props.name}'s Profile</Text>
+			<ScrollView style={styles.container}>
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>{this.user.name}'s Profile</Text>
 				</View>
-				<View style={style.formText}>
-					<Text>Description</Text>
+				<View>
+					<Text style={styles.contentHeader}>Description:</Text>
 				</View>
-				<View style={style.formText}>
-					<Text>Interests</Text>
+				<ScrollView style={styles.descriptionBox}>
+					<Text style={styles.content}> {this.user.description}</Text>
+				</ScrollView>
+				<View>
+					<Text style={styles.contentHeader}>Interests:</Text>
 				</View>
-				<View style={style.formText}>
-					<Text>History</Text>
+				<ScrollView style={styles.descriptionBox}>
+					{this.user.interests.map(category => (
+						<Text style={styles.content}>
+							{category}
+						</Text>
+					))}
+				</ScrollView>
+				<View>
+					<Text style={styles.contentHeader}>History:</Text>
 				</View>
-				<View style={style.formText}>
-					<Text>Upcoming</Text>
+				<ScrollView style={styles.descriptionBox}>
+					{this.user.history.map(event => (
+						<Button
+							title = {event.details.title}
+							onPress={() => Alert.alert('Will direct to event page later!')}
+						>
+						</Button>
+					))}
+				</ScrollView>
+				<View>
+					<Text style={styles.contentHeader}>Upcoming:</Text>
 				</View>
-			</View>
+				<ScrollView style={styles.descriptionBox}>
+					{this.user.upcoming.map(event => (
+						<Button
+							title = {event.details.title}
+							onPress={() => Alert.alert('Will direct to event page later!')}
+						>
+						</Button>
+					))}
+				</ScrollView>
+			</ScrollView>
 		);
     }
 }
@@ -46,14 +105,30 @@ const styles = StyleSheet.create({
 	contentContainer: {
 	  paddingTop: 30,
 	},
-	title: {
-	  fontSize: 19,
-	  fontWeight: 'bold',
+	titleContainer: {
+	  backgroundColor: '#96CA92',
+	  borderRadius: 10
 	},
-	formText: {
+	title: {
+	  fontSize: 20,
+	  fontWeight: 'bold',
+	  textAlign: 'center',
+	  color: 'white'
+	},
+	contentHeader: {
 		color: 'black',
 		fontSize: 20,
-		textAlign: 'center'
+		textAlign: 'left'
+	},
+	content: {
+		color: 'black', 
+		fontSize: 15,
+		textAlign: 'left'
+	},
+	descriptionBox: {
+		backgroundColor: '#E4EBE3', 
+		borderRadius: 10,
+		height: 100
 	}
   });
   
