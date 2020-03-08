cc15a694e02a5f21e1296eb82a2b3d98be500ebf
zicongmo <32829031+zicongmo@users.noreply.github.com>
2020-03-08 15:57:40 -0700

    Add new categories (#23)

--- Spur/constants/categories.js
+++ Spur/constants/categories.js
@@ -8,8 +8,31 @@ const CATEGORIES = [
         id: 5
       },
       {
+        name: "English",
+        id: 7
+      },
+      {
         name: "Mathematics",
         id: 6
+      },
+    ]
+  },
+  {
+
+    name: "Entertainment",
+    id: 2000,
+    children: [
+      {
+        name: "Movies",
+        id: 2002
+      },
+      {
+        name: "TV",
+        id: 2003
+      },
+      {
+        name: "Video Games",
+        id: 2001
       }
     ]
   },
@@ -18,12 +41,42 @@ const CATEGORIES = [
     id: 1,
     children: [
       {
+        name: "Boba",
+        id: 3
+      },
+      {
         name: "Coffee",
         id: 2
       },
       {
-        name: "Boba",
-        id: 3
+        name: "Dining Hall",
+        id: 4
+      },
+      {
+        name: "Fruit",
+        id: 8
+      },
+      {
+        name: "Hydration",
+        id: 9
+      }
+    ]
+  },
+  {
+    name: "Software Engineering",
+    id: 1000,
+    children: [
+      {
+        name: "Design Patterns",
+        id: 1001
+      },
+      {
+        name: "Scrum",
+        id: 1002
+      },
+      {
+        name: "UML",
+        id: 1003
       }
     ]
   }
